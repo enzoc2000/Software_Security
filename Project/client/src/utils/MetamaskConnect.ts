95ff147e09f97ff17492c66752d12a8728124c24
Ema De M <47746088+Raevaen@users.noreply.github.com>
2025-05-15 19:58:02 +0200

    add connect wallet function

--- Project/client/src/utils/MetamaskConnect.ts
+++ Project/client/src/utils/MetamaskConnect.ts
@@ -17,3 +17,17 @@ export default async function connectToMetamask(): Promise<void> {
     console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
   }
 }
+import { ethers } from 'ethers';
+
+async function connectWallet() {
+  if (window.ethereum) {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    //TODO lookup documentation for available method names
+    await provider.send('eth_requestAccounts', []);
+    const signer = await provider.getSigner();
+    const address = await signer.getAddress();
+    return { provider, signer, address };
+  } else {
+    throw new Error('MetaMask is not installed');
+  }
+}
