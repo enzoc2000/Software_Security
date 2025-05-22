1c68ce224299b48bdddf5f5d21d286d67f1fd9ed
Ema De M <47746088+Raevaen@users.noreply.github.com>
2025-05-22 20:32:48 +0200

    remove extra Folders

--- BackEnd/Utils/validation.ts
+++
@@ -1,4 +0,0 @@
-// Funzione per validare i dati di input per l'emissione di CO2
-export function isValidCO2Amount(value: number): boolean {
-    return value >= 0 && value <= 100000; // esempio limite
-}
