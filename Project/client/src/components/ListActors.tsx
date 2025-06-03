4aeb376f44e6b4f5f18cc866f7b51a33c073dfd4
Ezcn56 <enzo.cingoli@gmail.com>
2025-06-03 19:05:11 +0200

    Modifiche correzione modello UserDTO al FE per via dell'aggiunta dell'email al DB

--- Project/client/src/components/ListActors.tsx
+++ Project/client/src/components/ListActors.tsx
@@ -21,7 +21,7 @@ export function ListActors() {
     return (
         <div className="flex flex-col w-screen h-screen place-items-center">
             <h1 className="text-2xl font-bold mb-4">Actors List</h1>
-            <div className="grid grid-cols-3 w-[80%] border-2 text-black text-2xl border-b-blue-900 border-t-red-800 border-r-red-800 border-l-blue-800" >
+            <div className="grid grid-cols-4 w-[80%] border-2 text-black text-2xl border-b-blue-900 border-t-red-800 border-r-red-800 border-l-blue-800" >
                 <div className="flex flex-wrap p-2 border-2 border-b-blue-900 border-t-red-800 border-r-red-800 border-l-blue-800  " >
                     Actor Name:
                 </div>
@@ -29,12 +29,15 @@ export function ListActors() {
                     Role:
                 </div>
                 <div className="flex flex-wrap p-2 border-2 border-b-blue-900 border-t-red-800 border-r-red-800 border-l-blue-800 " >
+                    Email:
+                </div>
+                <div className="flex flex-wrap p-2 border-2 border-b-blue-900 border-t-red-800 border-r-red-800 border-l-blue-800 " >
                     Wallet Balance:
                 </div>
             </div>
             <div className="grid w-[80%] border-2 text-black text-2xl border-b-blue-900 border-t-red-800 border-r-red-800 border-l-blue-800" >
                 {datiAttori.map((item: UserDTO) => (
-                    <Card key={item.id} name={item.name} role={item.role} walletBalance={item.wallet_balance!} />
+                    <Card key={item.id} name={item.name} role={item.role} email={item.email} walletBalance={item.wallet_balance!} />
                 ))}
             </div>
             <button className=" border-2 border-blue-800 p-2 rounded-lg m-5"
