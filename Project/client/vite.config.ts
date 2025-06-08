5f78e5cb6a383a65dfa64755d1c65c75dbf27486
Ema De M <47746088+Raevaen@users.noreply.github.com>
2025-06-08 11:35:50 +0200

    wip https certificate

--- Project/client/vite.config.ts
+++ Project/client/vite.config.ts
@@ -1,15 +1,24 @@
 import { defineConfig } from 'vite'
+import fs from 'fs'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 5173,
+    https: {
+      key: fs.readFileSync('localhost.key'),
+      cert: fs.readFileSync('localhost.crt'),
+    },
+  },
   preview: {
     host: true,
     port: 5173,
-    // https: {
-    //   key: 'path/to/your/key.pem',
-    //   cert: 'path/to/your/cert.pem',
-    // },
+    https: {
+      key: './localhost.key',
+      cert: './localhost.crt',
+    },
   },
   plugins: [
     react(),
