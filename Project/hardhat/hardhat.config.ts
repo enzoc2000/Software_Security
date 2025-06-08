f9d87cebf744d9f216add89db057efa1f12796a4
Ema De M <47746088+Raevaen@users.noreply.github.com>
2025-06-08 20:24:23 +0200

    add default deployer private key

--- Project/hardhat/hardhat.config.ts
+++ Project/hardhat/hardhat.config.ts
@@ -3,6 +3,7 @@ import "@nomicfoundation/hardhat-toolbox";
 import dotenv from "dotenv";
 dotenv.config();
 
+const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY ?? "7a18769fc1e450f623619bb54b67e118a2462ae5f8f4be8f066de5a77cfc3cf1"; // Default private key for testing purposes, replace with your own
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
@@ -10,7 +11,7 @@ const config: HardhatUserConfig = {
     besu: {
       url: "http://localhost:8545",
       chainId: 1338,
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY!],
+      accounts: [DEPLOYER_PRIVATE_KEY],
       gas: 12_000_000,
       gasPrice: 0x342770c0,
       timeout: 120000
